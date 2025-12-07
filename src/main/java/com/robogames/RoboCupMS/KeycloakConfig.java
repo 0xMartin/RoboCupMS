205b0f6198131dd13e9efae957eee16f9ee7ead6
0xMartin <martin.krcma1@gmail.com>
2025-12-07 14:37:55 +0100

    keycloak sync: on user delete

--- src/main/java/com/robogames/RoboCupMS/KeycloakConfig.java
+++ src/main/java/com/robogames/RoboCupMS/KeycloakConfig.java
@@ -1,12 +1,20 @@
 package com.robogames.RoboCupMS;
 
-
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakConfig {
 
     @Value("${keycloak.auth-server-url}")
     private String serverUrl;
+
+    @Value("${keycloak.client-id}")
+    private String clientId;
 
     @Value("${keycloak.realm}")
     private String realm;
@@ -21,8 +29,8 @@ public class KeycloakConfig {
     public Keycloak keycloak() {
         return KeycloakBuilder.builder()
                 .serverUrl(serverUrl)
-                .realm("master")
-                .clientId("admin-cli")
+                .realm(realm)
+                .clientId(clientId)
                 .grantType(OAuth2Constants.PASSWORD)
                 .username(adminUser)
                 .password(adminPassword)
